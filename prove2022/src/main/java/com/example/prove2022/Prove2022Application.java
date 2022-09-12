ec46f8f4c769d401fe5f7b2130e54bc1df98a969
Dong Seok Lee <dongseok.lee.log@gmail.com>
2022-09-12 17:05:49 +0900

    feat: 프로젝트 초기 설정

---
+++ prove2022/src/main/java/com/example/prove2022/Prove2022Application.java
@@ -0,0 +1,13 @@
+package com.example.prove2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prove2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prove2022Application.class, args);
+	}
+
+}
