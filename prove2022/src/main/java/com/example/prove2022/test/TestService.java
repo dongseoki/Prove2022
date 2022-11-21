4fd4a014693cc0d6c45f2d31fcd637db66ffbc83
Dong Seok Lee <dongseok.lee.log@gmail.com>
2022-11-21 10:39:22 +0900

    초기화중 현재 로깅 설정 이상... 상태

--- prove2022/src/main/java/com/example/prove2022/test/TestService.java
+++ prove2022/src/main/java/com/example/prove2022/test/TestService.java
@@ -1,11 +1,6 @@
 package com.example.prove2022.test;
 
 import com.example.prove2022.domain.TimeUnit;
-import com.jam2in.arcus.app.common.aop.ArcusCache;
-import com.jam2in.arcus.app.common.aop.ArcusCacheKey;
-import com.jam2in.arcus.app.common.aop.ArcusCacheKeyParameter;
-import com.jam2in.arcus.app.common.key.ArcusCacheKeyDate;
-import com.jam2in.arcus.app.common.recaching.ArcusRecachingType;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -31,8 +26,7 @@ public class TestService {
         }
     }
 
-    @ArcusCache
-    public String getTestData(@ArcusCacheKey String testKey){
+    public String getTestData(String testKey){
         return testDataMap.get(testKey);
 
     }
@@ -44,8 +38,7 @@ public class TestService {
 
     // guide 시작.
     // 1) APP_USER:case1#20220101
-    @ArcusCache(prefix="_USER", expireTime="10",keyDate= ArcusCacheKeyDate.KEY_DATE_DAY, recachingType = ArcusRecachingType.SUS)
-    public User getCase1(@ArcusCacheKey long id) {
+    public User getCase1(long id) {
         try {
             Thread.sleep(20);
         } catch (InterruptedException e) {
@@ -68,15 +61,12 @@ public class TestService {
 
 
 //    // 5) APP:user.UserService.case5@ARG0=<id=1,name=arcus>,ARG1=<*=1>
-    @ArcusCache
-    public User getCase5(
-            @ArcusCacheKeyParameter({"id", "name"}) User user,@ArcusCacheKey int userId) {
+    public User getCase5(User user, int userId) {
         return testUserMap.get(user.getId()+" "+user.getName());
     }
 
     // 6) APP:user.UserService.case6@ARG0=<*=[1,2,3]>
-    @ArcusCache
-    public List<User> getCase6(@ArcusCacheKey int[] userIds) {
+    public List<User> getCase6(int[] userIds) {
         List<User> li = new ArrayList();
         for (int userId :
                 userIds) {
@@ -95,9 +85,7 @@ public class TestService {
 //    public List<User> case9(@ArcusCacheKeyParameter("id") List<User> users) {...}
 
     // 10) APP:user.UserService.case10@ARG0=<id=1,name=arcus>
-    @ArcusCache
-    public User getCase10(@ArcusCacheKeyParameter({"id", "name"}) Map<String, Object> user
-    ) {
+    public User getCase10(Map<String, Object> user) {
         return testUserMap.get(""+user.get("id").toString() + " "+user.get("name").toString());
     }
     // 11) APP:user.UserService.case11@ARG0=<*={id=1,name=arcus}>
