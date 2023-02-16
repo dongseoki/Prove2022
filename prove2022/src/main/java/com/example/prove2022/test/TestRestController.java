9c8cc27c1f3cf5f0a99b9a7499c8cd31eb227d08
dongseoki <dongseok.lee.log@gmail.com>
2023-02-16 14:41:09 +0900

    params 테스트 완료.

--- prove2022/src/main/java/com/example/prove2022/test/TestRestController.java
+++ prove2022/src/main/java/com/example/prove2022/test/TestRestController.java
@@ -1,8 +1,10 @@
 package com.example.prove2022.test;
 
 import com.example.prove2022.test.domain.TestProduct;
+import com.example.prove2022.test.dtos.TestRequestDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,21 @@ public class TestRestController {
     @GetMapping("/product/{id}")
     public TestProduct getProduct(@PathVariable String id){
         TestProduct testProduct = TestProduct.builder().id(Long.valueOf(id)).name("test").build();
-
         return testProduct;
+    }
+
+    @GetMapping(value = "/param-test")
+    public String getProduct3(){
+        return "repl Nothing Success";
+    }
+    @GetMapping(value = "/param-test", params = {"repl=true"})
+    public String getProduct(@RequestBody TestRequestDto testRequestDto){
+
+        return "repl True Success" + testRequestDto;
+    }
+
+    @GetMapping(value = "/param-test", params = {"repl=false"})
+    public String getProduct2(){
+        return "repl False Success";
     }
 }
---
+++ prove2022/src/main/java/com/example/prove2022/test/dtos/TestRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.prove2022.test.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@ToString
+@Getter
+public class TestRequestDto {
+  private String name;
+}
